994e6087624ebcddcf92bc618e9e368cc4654c0b
Ward Bell <wardbell@hotmail.com>
2017-05-17 07:48:38 -0700

    feat: refresh on click if expired (#1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/hero.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/map';
+
+import { Hero } from './hero';
+
+export interface cachedResponse<T> {
+  fetching: boolean;
+  expiration: number;
+  data: T;
+}
+
+@Injectable()
+export class HeroService {
+
+  private expireAfter = 5000; // data expire after this period of time (ms).
+
+  private heroesSubject = new BehaviorSubject<cachedResponse<Hero[]>>({
+    fetching: false,
+    expiration: 0,
+    data: undefined
+  });
+
+  private heroes = this.heroesSubject.asObservable();
+
+  private url = 'hero.json';
+
+  constructor(private http: Http) { }
+
+  getHeroes(): Observable<cachedResponse<Hero[]>> {
+    this.heroesSubject.do(pkg => {
+      if (pkg.fetching) {
+        console.log('fetching...');
+      } else if (pkg.expiration > Date.now()) {
+        console.log('returned cached because within expiration window');
+      } else {
+        // emit a new package with the same cached data that shows we're fetching
+        this.heroesSubject.next({...pkg, ...{fetching: true}});
+
+        // then get data from the server
+        this.http.get(this.url).map(res => res.json())
+          .map(data => ({
+            fetching: false,
+            expiration: Date.now() + this.expireAfter,
+            data
+          }))
+          .do(newPkg => console.log('fetched data', newPkg))
+          .subscribe(
+            newPkg => this.heroesSubject.next(newPkg)
+            // Todo: add error handling; set fetching false!
+          );
+      }
+    })
+    .first() // execute only once; the returned value is irrelevant
+    .subscribe();
+
+    return this.heroes;
+  }
+}
