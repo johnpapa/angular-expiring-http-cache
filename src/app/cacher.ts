4ffff64176c0235e2874061fc32409b139bf0ccf
Ward Bell <wardbell@hotmail.com>
2017-05-19 13:54:22 -0700

    feat(Cacher): rewrite with caching-fns (#6)

--- src/app/cacher.ts
+++ src/app/cacher.ts
@@ -1,109 +1,47 @@
 import { Observable } from 'rxjs/Observable';
+import * as cacheFns from './caching-fns';
+
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/first';
 
-/////// CachedResponse<T> ////////
-
-/** CachedResponse returned by Cacher `get` methods. */
-export class CachedResponse<T> {
-  constructor(
-    public data: T,
-    public expiration = 0,
-    /** True if a "fetch" (execution of the source) is in progress. */
-    public fetching = false,
-    /** Error from source if it fails */
-    public error?: any) { }
-}
-
-/////// Cacher ////////
-
 export class Cacher<T> {
+
+  /** Whether to log Cacher activity to console. For debugging/demos. */
+  static get verbose() { return cacheFns._verbose; };
+  static set verbose(value: boolean) { cacheFns.setVerbose(value); };
 
   /** Cached values expire after this period (ms) by default */
   static defaultExpirationPeriod = 30000;
 
-  /** Whether to log Cacher activity to console. For debugging/demos. */
-  static verbose = false;
+  /** Observable of cached values */
+  readonly cache: Observable<T>;
 
-  /** Observable representation of the private caching subject */
-  private readonly cache: Observable<CachedResponse<T>>;
+  private nextSubject = new ReplaySubject<boolean>();
 
   /**
    * Create instance of a Cacher which can cache and refresh an observable of type T
    *
    * @param {Observable<T>} source The observable source of values of type T
-   * @param { number } expireAfter Cached values expire after this period.
-   *   Default is `Cacher.defaultExpirationPeriod`.
-   * @param { BehaviorSubject<CachedResponse<T>>} [subject] Optional subject that maintains cached value;
-   *   Creates its own subject if not defined.
+   * @param {Function} [fetched] Optional callback when cacher has fetched from the source
+   * @param {number} [expirationPeriod=defaultExpirationPeriod] Expiration window.
    */
   constructor(
     public readonly source: Observable<T>,
-    public readonly expireAfter = Cacher.defaultExpirationPeriod,
-    private readonly subject?: BehaviorSubject<CachedResponse<T>>
+    public fetched = () => {},
+    public readonly expirationPeriod = Cacher.defaultExpirationPeriod
   ) {
-    if (!this.subject) {
-      this.subject = new BehaviorSubject(new CachedResponse<T>(undefined));
-    }
-    this.cache = this.subject.asObservable(); // because shouldn't expose subject directly
+    this.cache = cacheFns.onDemandCache(source, this.nextSubject, fetched, expirationPeriod);
   }
 
   /**
-   * Returns the observable of cached values.
-   * If the cached value expired, initiates a fetch from source.
-   * Can force a fetch even if the cached value has not expired.
+   * update cache by force or if the cache has expired.
    *
-   * @param {boolean} [force=false] forces a fetch that updates the cached observable.
+   * @param {boolean} [force=false] Whether to force update from source
    */
-  get(force = false): Observable<CachedResponse<T>> {
-    this.subject
-      .first() // execute only once
-      .do(cr => {
-        if (cr.fetching) {
-          log('Fetching ...');
-        } else if (force || Date.now() > cr.expiration ) {
-          this.getFromSource(cr);
-        } else {
-          log('Using cached data');
-        }
-      })
-      .subscribe(null, null, () => log('get.do() completed'));
-
-    return this.cache;
-  };
-
-  /**
-   * Returns the observable of cached values.
-   * A future call of `get()` may update this cache.
-   */
-  getFromCache(): Observable<CachedResponse<T>> { return this.cache; }
-
-  private getFromSource(cr: CachedResponse<T>) {
-    // Notify that we're fetching ...
-    cr.fetching = true;
-    this.subject.next(cr);
-
-    // then fetch the data
-    this.source.first().subscribe(
-      data => {
-        const newCr = new CachedResponse<T>(data, Date.now() + this.expireAfter);
-        log('Fetched fresh data', newCr);
-        this.subject.next(newCr);
-      },
-
-      error => {
-        const newCr = new CachedResponse<T>(cr.data, 0, false, error);
-        log('Fetch failed', error);
-        this.subject.next(newCr);
-      },
-
-      () => log('Fetch observable completed')
-    );
+  update(force = false) {
+    this.nextSubject.next(force);
   }
-}
-
-function log(...args) {
-  if (Cacher.verbose) { console.log.apply(null, args); }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,7 +11,7 @@ import 'rxjs/add/operator/let';
 import 'rxjs/add/operator/map';
 
 import { Cacher } from './cacher';
-import { demandCacher } from './cacher2';
+import { onDemandCache } from './caching-fns';
 
 import { CountDownService } from './countdown.service';
 
@@ -28,53 +28,32 @@ export class DataService {
   private heroCacher: Cacher<Hero[]>;
   private villainCacher: Cacher<Villain[]>;
 
-  private nextHero = new ReplaySubject<boolean>();
-  private hero2: Observable<Hero[]>;
-  private hero2Started = false;
-
   constructor(
     private countdownService: CountDownService,
     private http: Http) {
 
-    Cacher.verbose = true; // So we can toggle console logs
+    Cacher.verbose = true; // So we can see console logs
+
+    const heroSource = <Observable<Hero[]>>this.http.get(`/heroes.json`)
+      .map((response: Response) => response.json());
 
     const villainSource = <Observable<Villain[]>>this.http.get(`/villains.json`)
       .delay(5000) // simulate latency
       .map((response: Response) => response.json());
 
-    const heroSource = <Observable<Hero[]>>this.http.get(`/heroes.json`)
-      .map((response: Response) => response.json());
-
-    this.villainCacher = new Cacher<Villain[]>(villainSource);
-    this.heroCacher = new Cacher<Hero[]>(heroSource);
-
-    this.hero2 = demandCacher(
-      heroSource,
-      this.nextHero,
-      countdownService.hero2CountDownReset);
-  }
-
-  getHeroes2(force = false) {
-    this.countdownService.startHero2Counter();
-    force = force || !this.hero2Started;
-    this.hero2Started = true;
-    this.nextHero.next(force);
-    return this.hero2;
+    this.heroCacher = new Cacher<Hero[]>(heroSource, countdownService.heroCountDownReset);
+    this.villainCacher = new Cacher<Villain[]>(villainSource, countdownService.villainCountDownReset);
   }
 
   getHeroes(force = false) {
     this.countdownService.startHeroCounter(this.heroCacher);
-
-    return this.heroCacher.get(force)
-        .filter(cr => !cr.fetching)
-        .map(cr => cr.data);
+    this.heroCacher.update(force);
+    return this.heroCacher.cache;
   }
 
   getVillains(force = false) {
     this.countdownService.startVillainCounter(this.villainCacher);
-
-    return this.villainCacher.get(force)
-      .filter(cr => !cr.fetching)
-      .map(cr => cr.data);
+    this.villainCacher.update(force);
+    return this.villainCacher.cache;
   }
 }
